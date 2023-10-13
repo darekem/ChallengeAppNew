f89d2d62f8e8ed2ac1b31e6d99aac237e20ffa88
Darek Miler <darekmiler@wp.pl>
2023-10-13 15:54:40 +0200

    zadanie domowe dzien 4

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿var name = "Ewa";
+var gender = "K";
+var age = 33;
+if (gender == "K" && age < 30)
+{
+    Console.WriteLine("kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa lat 33");
+}
+else if (gender == "M" && age < 18)
+{
+    Console.WriteLine("niepełnoletni mężczyzna");
+}
